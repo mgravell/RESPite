cdf4e7385281c4ba1c7fbb1988af0edd24ece0fb
mgravell <marc.gravell@gmail.com>
2020-02-11 08:04:39 +0000

    split benchmark into sync vs async
    |            Method |     Mean |    Error |   StdDev | Ratio | Gen 0 | Gen 1 | Gen 2 | Allocated |
    |------------------ |---------:|---------:|---------:|------:|------:|------:|------:|----------:|
    |      SERedisAsync | 86.79 us | 0.717 us | 0.671 us |  1.00 |     - |     - |     - |     360 B |
    |      BedrockAsync | 80.79 us | 1.320 us | 1.235 us |  0.93 |     - |     - |     - |     865 B |
    | DirectSocketAsync | 68.66 us | 0.933 us | 0.872 us |  0.79 |     - |     - |     - |     728 B |
    |                   |          |          |          |       |       |       |       |           |
    |           SERedis | 85.05 us | 0.515 us | 0.430 us |  1.00 |     - |     - |     - |     184 B |
    |           Bedrock | 79.81 us | 0.921 us | 0.816 us |  0.94 |     - |     - |     - |     656 B |
    |      DirectSocket | 58.53 us | 0.315 us | 0.279 us |  0.69 |     - |     - |     - |         - |

--- tests/RunProfiler/Program.cs
+++ tests/RunProfiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Bedrock.Framework;
 using BedrockRespProtocol;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +10,7 @@ using Resp.Redis;
 using StackExchange.Redis;
 using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace RunProfiler
@@ -20,22 +22,44 @@ namespace RunProfiler
 }
 
 [MemoryDiagnoser]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
 public class RedisPingPong : IAsyncDisposable
 {
     private ConnectionMultiplexer _muxer;
     private IServer _server;
-    private RespConnection _protocol;
+    private RespConnection _bedrock, _directSocket;
+    private Socket _socket;
     private ConnectionContext _connection;
 
-    [Benchmark]
-    public Task SERedis() => _server.PingAsync();
+    [BenchmarkCategory("Async")]
+    [Benchmark(Baseline = true)]
+    public Task SERedisAsync() => _server.PingAsync();
 
+    [BenchmarkCategory("Async")]
     [Benchmark]
-    public Task Bedrock() => _protocol.PingAsync().AsTask();
+    public Task BedrockAsync() => _bedrock.PingAsync().AsTask();
+
+    [BenchmarkCategory("Async")]
+    [Benchmark]
+    public Task DirectSocketAsync() => _directSocket.PingAsync().AsTask();
+
+    [BenchmarkCategory("Sync")]
+    [Benchmark(Baseline = true)]
+    public void SERedis() => _server.Ping();
+
+    [BenchmarkCategory("Sync")]
+    [Benchmark]
+    public void Bedrock() => _bedrock.Ping();
+
+    [BenchmarkCategory("Sync")]
+    [Benchmark]
+    public void DirectSocket() => _directSocket.Ping();
+
 
     public ValueTask DisposeAsync()
     {
         _muxer?.Dispose();
+        _socket?.Dispose();
         return _connection == null ? default : _connection.DisposeAsync();
     }
 
@@ -48,7 +72,7 @@ public class RedisPingPong : IAsyncDisposable
             EndPoints = { endpoint }
         });
         _server = _muxer.GetServer(endpoint);
-        await SERedis();
+        await SERedisAsync();
 
         var serviceProvider = new ServiceCollection().BuildServiceProvider();
         var client = new ClientBuilder(serviceProvider)
@@ -56,9 +80,12 @@ public class RedisPingPong : IAsyncDisposable
             .Build();
 
         _connection = await client.ConnectAsync(endpoint);
+        _bedrock = new RespBedrockProtocol(_connection);
+        await BedrockAsync();
 
-        _protocol = new RespBedrockProtocol(_connection);
-
-        await Bedrock();
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _socket.Connect(endpoint);
+        _directSocket = RespConnection.Create(_socket);
+        await DirectSocketAsync();
     }
 }
